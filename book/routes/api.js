e29a896c0be25447fc8e4cc9bb721bfa7b895388
kodai <kodaiadvance@icloud.com>
2024-04-30 19:06:42 +0900

    CORSの仕組みと対策

--- book/routes/api.js
+++ book/routes/api.js
@@ -2,6 +2,22 @@ const express = require("express"); //Expressのルーティング用オブジ�
 
 const router = express.Router();
 
+const allowList = [
+    "http://localhost:3000",
+    "http://site.example:3000"
+];
+
+router.use((req, res, next) => {
+    if (req.headers.origin && allowList.includes(req.headers.origin)) {
+        res.header("Access-Control-Allow-Origin", req.headers.origin);
+    }
+    
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Headers", "X-Token");
+    }
+    next();
+});
+
 router.get("/", (req, res) => {
     res.setHeader("X-Timestamp", Date.now()); //ヘッダをレスポンスに追加する処理
     let message = req.query.message;
