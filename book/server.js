4f80dbf3e11aecce39bab086892fea49302f47ee
kodai <kodaiadvance@icloud.com>
2024-05-02 21:30:11 +0900

    XSS, CSPの検証

--- book/server.js
+++ book/server.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const api = require("./routes/api"); //api.jsを読み込む
+const crypto = require("crypto");
 const app = express();
 const port = 3000;
+
+app.set("view engine", "ejs");
 
 app.use(express.static("public")); //頻繁に使用する関数の指定
 
@@ -9,6 +12,19 @@ app.use("/api", api);
 
 app.get("/", (req, res, next) => {
     res.end("Top Page");
+});
+
+app.get("/csp", (req, res) => {
+    const nonceValue = crypto.randomBytes(16).toString("base64"); //毎回ランダムな文字列生成
+
+    res.header("Content-Security-Policy",
+     `script-src 'nonce-${nonceValue}' 'strict-dynamic';` +
+     "object-src 'none';" +
+     "base-uri 'none';" +
+     "require-trusted-types-for 'script';"
+    ); // 生成した値をCSPヘッダに指定
+
+    res.render("csp", { nonce: nonceValue }); //生成したnonce値をHTMLに渡す
 });
 
 app.listen(port, () => {
